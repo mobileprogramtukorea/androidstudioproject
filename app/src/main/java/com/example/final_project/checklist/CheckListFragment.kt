e81dec43c2e82059f8e3df6f22603827f993acc8
김소진 <dfsa356@naver.com>
2024-06-24 12:16:31 +0900

    feat : make CheckList Page and MakeCheckList Page

--- app/src/main/java/com/example/final_project/checklist/CheckListFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.final_project.checklist
-
-import androidx.fragment.app.Fragment
-import com.example.final_project.R
-
-class CheckListFragment: Fragment(R.layout.fragment_checklist) {
-}
